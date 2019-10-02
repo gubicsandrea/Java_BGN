0d0a76ea26f3fa521adfb49cac1514f984130287
Gubics Andrea <gubics.andrea@gmail.com>
2019-10-02 12:22:47 +0200

    Metódus bevezető: metódusok használata

---
+++ multiplier/src/main/java/methodexample/Main.java
@@ -0,0 +1,13 @@
+package methodexample;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 123;
+        Scanner scanner = new Scanner(System.in);
+        int number2 = scanner.nextInt(3);
+        System.out.println(number2);
+    }
+}
